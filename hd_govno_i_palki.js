0d07dfe7a8faf509b46a80ad336605350ff6eb1c
grenudi <idunerg@gmail.com>
2018-10-30 18:16:23 +0300

    Update hd_govno_i_palki.js

--- hd_govno_i_palki.js
+++ hd_govno_i_palki.js
@@ -28,18 +28,10 @@ function riddOf7 ( ...list ){
     
     // inputs[4].onpaste = eRidOf7;
     list.forEach(x=>x.onpaste = eRidOf7);
-    document.getElementsByTagName("button")[0].click();
 };
 const inputs = document.querySelectorAll(".ng-pristine.ng-untouched.ng-valid.ng-scope.input-text.ng-empty");
 if(inputs.length > 0){
     riddOf7(inputs[4],inputs[3]);
-}
-
-function doCopyable(...elems){
-<td style="width: 220px"><a href="unsafe:telnet://194.186.92.56" class="ng-binding">194.186.92.56</a> <button class="btnCopy" data-clipboard-text="194.186.92.56"><i class="fa fa-clipboard" aria-hidden="true"></i></button> <button class="btnCopy" style="width:32px" data-clipboard-text="hotkey://tsterm?switch=194.186.92.56&amp;echelon=0">SSH</button></td>   
-}
-
-const ipses = document.getElementById("tableOnline").firstChild.getElementsByTagName("tr");
-if(ipses){
-    doCopyable(ip[1].lastChild)
+}else{
+    console.error("no inputs on this page");
 }
