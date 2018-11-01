b8e8daa872fc0e00792a7218a168b75ad00c5d1b
grenudi <idunerg@gmail.com>
2018-11-01 19:21:40 +0300

    Update hd_govno_i_palki.js

--- hd_govno_i_palki.js
+++ hd_govno_i_palki.js
@@ -1,4 +1,8 @@
 function addbr (){
+    const tmp_area = document.getElementById("submit_ticket_form");
+    if(!tmp_area){
+        return console.log("No textarea on the page!");
+    }
     let bttn = document.getElementById("submit_ticket_form")
     let area = document.getElementById("comment_text")
     let abbr = function(){
@@ -7,11 +11,6 @@ function addbr (){
     }
     bttn.onmouseover = abbr
 };
-
-const tmp_area = document.getElementById("submit_ticket_form");
-if(tmp_area){
-    addbr();
-}
 
 function riddOf7 ( ...list ){
   const inputs = document.querySelectorAll(".ng-pristine.ng-untouched.ng-valid.ng-scope.input-text.ng-empty");
@@ -38,27 +37,32 @@ function riddOf7 ( ...list ){
   list.forEach(x=>x.onpaste = eRidOf7);
 };
 
-function guidMacSn (){
+function notification(anchor, msg){
+  
+};
+function guidMacSn ()
+{
+  const sbmt = document.getElementsByTagName("button")[0];
   const inputs = document.querySelectorAll(".ng-pristine.ng-untouched.ng-valid.ng-scope.input-text.ng-empty");
-
   if(inputs.length <= 0 && !list)
     return console.error("NO INPUTS on the page");
 
-  list.push(inputs[0]);
-
-  const sbmt = document.getElementsByTagName("button")[0];
   const eHandler = function(e){
       e.preventDefault();
       let input = (e.clipboardData || window.clipboardData).getData('text');
-      //alert(JSON.stringify(input));
-
-      e.target.value = input.replace(/\D/gm,"").replace(/^(?:7|8)/,"");
+      input = wtfIsThis(input);
+      if(!input){
+        notification(e.target, "Это не МАК, не ГУИД и не Серийный номер! тогда зачем ты мне даешь это ?");
+        return;
+      }
+      alert(JSON.stringify(input));
+      e.target.value = input;
       e.target.select();
       document.execCommand("copy");
       sbmt.click();
   }
   const wtfIsThis = function(str){
-    return cutToMatchMac || cutToMatchGuid || cutToMatchSn;
+    return cutToMatchMac(str) || cutToMatchGuid(str) || cutToMatchSn(str);
   }
   const cutToMatchMac = function(str){
     str = str.replace(/\W/gm,"");
@@ -68,16 +72,23 @@ function guidMacSn (){
     return str;
   }
   const cutToMatchGuid = function(str){
+    str = str.replace(/\W/gm,"");
+    if(str.length !== 32)
+      return undefined;
     return str;
   }
   const cutToMatchSn = function(str){
+    str = str.replace(/[\W,_]/gm,"");
+    if(str.length !== 16)
+      return undefined;
     return str;
   }
-  // inputs[4].onpaste = eRidOf7;
-  list.forEach(x=>x.onpaste = eRidOf7);
+
+  inputs[0].onpaste = eHandler;
 }
 
 switch(location.href){
   case "https://fttb.bee.vimpelcom.ru/ptn/ng_ptn#/queues": riddOf7(); break;
   case "https://fttb.bee.vimpelcom.ru/ptn/ng_ptn#/search-tv-equipment": guidMacSn(); break;
+  default: addbr(); break;
 }
