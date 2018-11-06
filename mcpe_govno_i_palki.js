87b0c5356d2d7e9b5bb9e5052ff67f7e57b8ae43
grenudi <idunerg@gmail.com>
2018-11-06 19:02:10 +0300

    Update mcpe_govno_i_palki.js

--- mcpe_govno_i_palki.js
+++ mcpe_govno_i_palki.js
@@ -1,27 +1,6 @@
-function riddOf7 ( field ){
-    const sbmt = document.getElementById("btnSearch_btn");
-    const eRidOf7 = function(e){
-        e.preventDefault();
-        let input = (e.clipboardData || window.clipboardData).getData('text');
-        //alert(JSON.stringify(input));
-        e.target.value = input
-                            .replace(/\W/gm,"")
-                            .split("")
-                            .reduce((acc,x,i)=>{
-                                acc+=x;
-                                if((i+1)%2===0){
-                                    acc+="-"   
-                                }
-                            });
-        e.target.select();
-        document.execCommand("copy");
-        // sbmt.click();
-    }
-    field.onpaste = eRidOf7;
-};
-const input = document.getElementById("tbDeviceID");
-if(input){
-    riddOf7(input);
-}else{
-    console.error("no inputs on this page");
+let tmp = document.getElementById("tblLeftMenu");
+if(tmp){
+    tmp = tmp.getElementsByTagName("span");
+    tmp[1].innerHTML = "Поскрести По Сусекам";
+    tmp[2].innerHTML = "Высер";
 }
